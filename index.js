c18c6c4f1ef891e9f88fef273cb6b0cd34c12ca5
akshit.mrski <akshit.mrski@gmail.com>
2025-02-17 17:52:35 +0530

    Update server routing to serve index.html instead of welcome.html

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "welcome.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get(
